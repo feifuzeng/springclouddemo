da0bbd8c24c666e988bf259b8af2304c95749abb
feifuzeng <feifz@tydic.com>
2018-09-04 11:28:16 +0800

    add Hystrix

--- eureka-consumer-feign/src/main/java/com/feifz/eurekaconsumerfeign/service/SchedualServiceHi.java
+++ eureka-consumer-feign/src/main/java/com/feifz/eurekaconsumerfeign/service/SchedualServiceHi.java
@@ -1,11 +1,13 @@
 package com.feifz.eurekaconsumerfeign.service;
 
+import com.feifz.eurekaconsumerfeign.service.impl.SchedualServiceHiHystric;
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "eureka-provider")
+@FeignClient(value = "eureka-provider",fallback = SchedualServiceHiHystric.class)
 public interface SchedualServiceHi {
 
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
