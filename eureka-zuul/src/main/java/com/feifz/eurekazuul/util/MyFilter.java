fb790345f69733b288d0605fc579c173ea91c968
feifuzeng <feifz@tydic.com>
2018-09-04 13:53:48 +0800

    add zuul

---
+++ eureka-zuul/src/main/java/com/feifz/eurekazuul/util/MyFilter.java
@@ -0,0 +1,56 @@
+package com.feifz.eurekazuul.util;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+import lombok.extern.log4j.Log4j2;
+
+/**
+ * @author feifz
+ * @version 1.0.0
+ * @Description TOOD
+ * @Date 2018/9/4 13:41
+ */
+@Component
+@Log4j2
+public class MyFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        log.info(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
+        Object accessToken = request.getParameter("token");
+        if(accessToken == null) {
+            log.warn("token is empty");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("token is empty");
+            }catch (Exception e){}
+
+            return null;
+        }
+        log.info("ok");
+        return null;
+    }
+}
